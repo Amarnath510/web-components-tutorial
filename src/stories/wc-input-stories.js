b6074f3ec1bf95082c6aa6aaae114ab621bdc63b
Amarnath <camarnath@castlighthealth.com>
2021-02-22 07:27:39 +0530

    Update wc-input-stories.js

--- src/stories/wc-input-stories.js
+++ src/stories/wc-input-stories.js
@@ -2,7 +2,7 @@ const wcInputStoriesTemplate = document.createElement('template');
 wcInputStoriesTemplate.innerHTML = `
   <wc-story-header title="Input"></wc-story-header>
 
-  <wc-input no-title="true"></wc-input>
+  <wc-input no-title="true" max-length="10"></wc-input>
 
   <wc-story-attr-header title="Component Inputs"></wc-story-attr-header>
 
@@ -14,9 +14,13 @@ wcInputStoriesTemplate.innerHTML = `
     <span slot="title">Size</span>
   </wc-dropdown>
 
+  <wc-dropdown id="maxLength" options="10|20|30">
+    <span slot="title">Max Length</span>
+  </wc-dropdown>
+
   <wc-story-attr-header title="Component Outputs"></wc-story-attr-header>
 
-  <wc-output-properties>
+  <wc-output-properties id="typeEvent">
     <span slot="item">Type Event</span>
   </wc-output-properties>
 `;
@@ -33,17 +37,29 @@ class WCInputStories extends HTMLElement {
   }
 
   addEvents() {
-    const inputTypeEle = this.shadowRoot.getElementById('inputType');
+    const inputTypeOptionsEle = this.shadowRoot.getElementById('inputType');
     const sizesEle = this.shadowRoot.getElementById('sizes');
+    const maxLengthEle = this.shadowRoot.getElementById('maxLength');
+    const wcInputEle = this.shadowRoot.querySelector('wc-input');
+    const typeEventEle = this.shadowRoot.querySelector('#typeEvent')
+    const typedContentEle = document.createElement('span')
+    typedContentEle.setAttribute('id', 'typedContent');
+    typeEventEle.shadowRoot.append(typedContentEle)
 
-    inputTypeEle.addEventListener('optionChanged', e => {
-      const wcInputEle = this.shadowRoot.querySelector('wc-input');
+    inputTypeOptionsEle.addEventListener('optionChanged', e => {
       wcInputEle.setAttribute('type', e.detail);
     });
 
     sizesEle.addEventListener('optionChanged', e => {
-      const wcInputEle = this.shadowRoot.querySelector('wc-input');
       wcInputEle.setAttribute('size', e.detail);
+    });
+    
+    maxLengthEle.addEventListener('optionChanged', e => {
+      wcInputEle.setAttribute('max-length', e.detail);
+    });
+
+    wcInputEle.addEventListener('inputChanged', e => {
+      typedContentEle.innerHTML = e.detail;
     });
   }
 }
